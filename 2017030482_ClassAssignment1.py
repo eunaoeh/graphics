91bff2fc124b77005cb843edf6d302a9f07fb71a
eunjinheohy <eunjinhh131@gmail.com>
2019-05-11 20:20:16 +0900

    BLENDER project

--- 2017030482_ClassAssignment1.py
+++ 2017030482_ClassAssignment1.py
@@ -51,7 +51,7 @@ def render():
     
     r = np.sqrt(np.square(eye_x-at_x)+np.square(eye_y-at_y)+np.square(eye_z-at_z))
     drawFrame()
-    drawPerson()
+    drawUnitCube()
 
         
 def cursor_callback(window, xpos, ypos):
@@ -334,91 +334,41 @@ def drawFrame():
         glVertex3fv(np.array([-1.8+(i*.2), 0., 1.8]))
         glEnd()
 
-def drawCube():
-    glPolygonMode( GL_FRONT_AND_BACK, GL_LINE )
+def drawUnitCube():
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
     glBegin(GL_QUADS)
-    glVertex3f( 1.0, 1.0,-1.0)
-    glVertex3f(-1.0, 1.0,-1.0)
-    glVertex3f(-1.0, 1.0, 1.0)
-    glVertex3f( 1.0, 1.0, 1.0)
-    
-    glVertex3f( 1.0,-1.0, 1.0)
-    glVertex3f(-1.0,-1.0, 1.0)
-    glVertex3f(-1.0,-1.0,-1.0)
-    glVertex3f( 1.0,-1.0,-1.0)
-    
-    glVertex3f( 1.0, 1.0, 1.0)
-    glVertex3f(-1.0, 1.0, 1.0)
-    glVertex3f(-1.0,-1.0, 1.0)
-    glVertex3f( 1.0,-1.0, 1.0)
-    
-    glVertex3f( 1.0,-1.0,-1.0)
-    glVertex3f(-1.0,-1.0,-1.0)
-    glVertex3f(-1.0, 1.0,-1.0)
-    glVertex3f( 1.0, 1.0,-1.0)
-    
-    glVertex3f(-1.0, 1.0, 1.0)
-    glVertex3f(-1.0, 1.0,-1.0)
-    glVertex3f(-1.0,-1.0,-1.0)
-    glVertex3f(-1.0,-1.0, 1.0)
-    
-    glVertex3f( 1.0, 1.0,-1.0)
-    glVertex3f( 1.0, 1.0, 1.0)
-    glVertex3f( 1.0,-1.0, 1.0)
-    glVertex3f( 1.0,-1.0,-1.0)
+    glColor3ub(255, 255, 255)
+    glVertex3f( 0.3, 0.3,-0.3)
+    glVertex3f(-0.3, 0.3,-0.3)
+    glVertex3f(-0.3, 0.3, 0.3)
+    glVertex3f( 0.3, 0.3, 0.3) 
+                             
+    glVertex3f( 0.3,-0.3, 0.3)
+    glVertex3f(-0.3,-0.3, 0.3)
+    glVertex3f(-0.3,-0.3,-0.3)
+    glVertex3f( 0.3,-0.3,-0.3) 
+                             
+    glVertex3f( 0.3, 0.3, 0.3)
+    glVertex3f(-0.3, 0.3, 0.3)
+    glVertex3f(-0.3,-0.3, 0.3)
+    glVertex3f( 0.3,-0.3, 0.3)
+                             
+    glVertex3f( 0.3,-0.3,-0.3)
+    glVertex3f(-0.3,-0.3,-0.3)
+    glVertex3f(-0.3, 0.3,-0.3)
+    glVertex3f( 0.3, 0.3,-0.3)
+ 
+    glVertex3f(-0.3, 0.3, 0.3) 
+    glVertex3f(-0.3, 0.3,-0.3)
+    glVertex3f(-0.3,-0.3,-0.3) 
+    glVertex3f(-0.3,-0.3, 0.3) 
+                             
+    glVertex3f( 0.3, 0.3,-0.3) 
+    glVertex3f( 0.3, 0.3, 0.3)
+    glVertex3f( 0.3,-0.3, 0.3)
+    glVertex3f( 0.3,-0.3,-0.3)
     glEnd()
 
-def drawCubeNotOrigin():
-    glBegin(GL_QUADS)
-    glVertex3f( 1.0, 0,-1.0)
-    glVertex3f(-1.0, 0,-1.0)
-    glVertex3f(-1.0, 0, 1.0)
-    glVertex3f( 1.0, 0, 1.0)
-    
-    glVertex3f( 1.0,-2.0, 1.0)
-    glVertex3f(-1.0,-2.0, 1.0)
-    glVertex3f(-1.0,-2.0,-1.0)
-    glVertex3f( 1.0,-2.0,-1.0)
-    
-    glVertex3f( 1.0, 0.0, 1.0)
-    glVertex3f(-1.0, 0.0, 1.0)
-    glVertex3f(-1.0,-2.0, 1.0)
-    glVertex3f( 1.0,-2.0, 1.0)
-    
-    glVertex3f( 1.0,-2.0,-1.0)
-    glVertex3f(-1.0,-2.0,-1.0)
-    glVertex3f(-1.0, 0.0,-1.0)
-    glVertex3f( 1.0, 0.0,-1.0)
-    
-    glVertex3f(-1.0, 0.0, 1.0)
-    glVertex3f(-1.0, 0.0,-1.0)
-    glVertex3f(-1.0,-2.0,-1.0)
-    glVertex3f(-1.0,-2.0, 1.0)
-    
-    glVertex3f( 1.0, 0.0,-1.0)
-    glVertex3f( 1.0, 0.0, 1.0)
-    glVertex3f( 1.0,-2.0, 1.0)
-    glVertex3f( 1.0,-2.0,-1.0)
-    glEnd()
-
-def drawSphere(numLats=12, numLongs=12):
-    for i in range(0, numLats + 1):
-        lat0 = np.pi * (-0.5 + float(float(i - 1) / float(numLats)))
-        z0 = np.sin(lat0)
-        zr0 = np.cos(lat0)
-        lat1 = np.pi * (-0.5 + float(float(i) / float(numLats)))
-        z1 = np.sin(lat1)
-        zr1 = np.cos(lat1)
-
-        # Use Quad strips to draw the sphere
-        glBegin(GL_QUAD_STRIP)
-        for j in range(0, numLongs + 1):
-            lng = 2 * np.pi * float(float(j - 1) / float(numLongs))
-            x = np.cos(lng)
-            y = np.sin(lng)
-            glVertex3f(x * zr0, y * zr0, z0)
-            glVertex3f(x * zr1, y * zr1, z1)
-        glEnd()
     
 def main():
     if not glfw.init():
